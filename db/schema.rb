fbae5974a35ad10cc4c2cb6b49de5737569bc0f4
longbb <longbuiba95@gmail.com>
2017-03-29 01:21:26 +0700

    Change logic in register

--- db/schema.rb
+++ db/schema.rb
@@ -10,20 +10,28 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170326152829) do
+ActiveRecord::Schema.define(version: 20170328175211) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
-  create_table "users", force: :cascade do |t|
+  create_table "customers", force: :cascade do |t|
     t.string   "fullname"
     t.string   "email"
-    t.string   "password_digest"
     t.string   "address"
     t.string   "phone_number"
     t.string   "status"
+    t.datetime "created_at",   null: false
+    t.datetime "updated_at",   null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string   "email"
+    t.string   "password_digest"
+    t.string   "status"
     t.datetime "created_at",      null: false
     t.datetime "updated_at",      null: false
+    t.integer  "customer_id"
   end
 
 end
